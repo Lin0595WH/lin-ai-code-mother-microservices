7a9ebf31f3a6e49d2db0a4597078c9b25a404e06
Lin0595WH <1803057925@qq.com>
2026-09-17 23:20:52 +0800

    feat: 1.指定jedis依赖，解决大模型对话缓存无法写入问题 2.优化截图工具类

--- lin-ai-code-screenshot/src/main/java/com/lin/linaicodemother/service/impl/ScreenshotServiceImpl.java
+++ lin-ai-code-screenshot/src/main/java/com/lin/linaicodemother/service/impl/ScreenshotServiceImpl.java
@@ -29,6 +29,8 @@ public class ScreenshotServiceImpl implements ScreenshotService {
 
     private final CosManager cosManager;
 
+    private final WebScreenshotUtils webScreenshotUtils;
+
     /**
      * 通用的截图服务，可以得到访问地址
      *
@@ -40,7 +42,7 @@ public class ScreenshotServiceImpl implements ScreenshotService {
         // 参数校验
         ThrowUtils.throwIf(CharSequenceUtil.isBlank(webUrl), ErrorCode.PARAMS_ERROR, "截图的网址不能为空");
         // 本地截图(压缩后的）
-        String localScreenshotPath = WebScreenshotUtils.saveWebPageScreenshot(webUrl);
+        String localScreenshotPath = webScreenshotUtils.saveWebPageScreenshot(webUrl);
         ThrowUtils.throwIf(CharSequenceUtil.isBlank(localScreenshotPath), ErrorCode.OPERATION_ERROR, "生成网页截图失败");
         try {
             // 上传图片到 COS
